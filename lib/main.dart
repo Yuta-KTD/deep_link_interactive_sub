287ab5ed34004ff246e782b91754b18be695875a
yuta.katada@enechange.co.jp <yuta.katada@enechange.co.jp>
2025-03-01 22:58:01 +0900

    完成

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:app_links/app_links.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MainApp());
@@ -10,37 +12,76 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('Hello World!'),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const SecondPage()),
-                  );
-                },
-                child: const Text('Go to Second Page'),
-              ),
-            ],
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: DeepLinkHandler(),
     );
   }
 }
 
-class SecondPage extends StatelessWidget {
-  const SecondPage({super.key});
+class DeepLinkHandler extends StatefulWidget {
+  const DeepLinkHandler({super.key});
+
+  @override
+  State<DeepLinkHandler> createState() => _DeepLinkHandlerState();
+}
+
+class _DeepLinkHandlerState extends State<DeepLinkHandler> {
+  final AppLinks _appLinks = AppLinks();
+  StreamSubscription<Uri>? _linkSubscription;
+  String _message = '待機中...';
+
+  @override
+  void initState() {
+    super.initState();
+    _initDeepLinkListener();
+  }
+
+  @override
+  void dispose() {
+    _linkSubscription?.cancel();
+    super.dispose();
+  }
+
+  Future<void> _initDeepLinkListener() async {
+    // ディープリンクを監視
+    _linkSubscription = _appLinks.uriLinkStream.listen(
+      (Uri uri) {
+        _handleDeepLink(uri);
+      },
+      onError: (error) {
+        setState(() {
+          _message = 'リスナーエラー: $error';
+        });
+      },
+    );
+  }
+
+  void _handleDeepLink(Uri uri) {
+    // fetchIdパラメータの確認
+    final host = uri.host;
+
+    if (host == 'fetchid') {
+      setState(() {
+        _message = 'fetchidを検出しました。別アプリを呼び出します...';
+      });
+      _launchDeepLink();
+    } else {
+      setState(() {
+        _message = '予期しない呼び出し: fetchidパラメータがありません';
+      });
+    }
+  }
 
   Future<void> _launchDeepLink() async {
-    final Uri url = Uri.parse('deepLinkMain://example.com?request_id=123');
-    if (!await launchUrl(url)) {
-      throw Exception('Could not launch $url');
+    const deepLinkUrl = 'deepLinkMainScheme://open?id=123';
+
+    try {
+      final uri = Uri.parse(deepLinkUrl);
+      await launchUrl(uri);
+    } catch (e) {
+      setState(() {
+        _message = 'エラーが発生しました: $e';
+      });
     }
   }
 
@@ -48,12 +89,28 @@ class SecondPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Second Page'),
+        title: const Text('ディープリンクデモ'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: _launchDeepLink,
-          child: const Text('Launch Deep Link'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('ディープリンクステータス:'),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                _message,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: _launchDeepLink,
+              child: const Text('手動で呼び出す'),
+            ),
+          ],
         ),
       ),
     );
